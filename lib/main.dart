9934193db246003794413a45ab41652ab670c4e7
Arnav Shah <arnavkshah2003@gmail.com>
2023-04-01 20:10:48 +0530

    added content to homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:handy_code/pages/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
+        
         ),
       ),
+      initialRoute: '/home',
+      routes: {
+        '/home':(context) => HomePage(),
+      },
     );
   }
 }
---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _selectedOption = '';
+
+  List<String> options = ['Set', 'C++', 'Java', 'Python'];
+  @override
+  void initState() {
+    super.initState();
+    _selectedOption = options[0];
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.green,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+                child: Text(
+              "SET   THE",
+              style: TextStyle(fontSize: 30),
+            )),
+            Center(child: Text("LANGUAGE", style: TextStyle(fontSize: 30))),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 7.5,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: DropdownButton<String>(
+                    dropdownColor: Colors.lightBlue.shade200,
+                    value: _selectedOption,
+                    items: options.map((String option) {
+                      return DropdownMenuItem<String>(
+                        value: option,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width / 3.5,
+                            ),
+                            Text(option),
+                          ],
+                        ),
+                        alignment: Alignment.center,
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedOption = newValue!;
+                      });
+                    },
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.lightBlue.shade200,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 7.5,
+                ),
+                GestureDetector(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blue),
+                    child: Center(
+                        child: Row(
+                      children: [
+                        SizedBox(width: 60),
+                        Text(
+                          "Customize snippet",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 20),
+                        Icon(Icons.arrow_forward)
+                      ],
+                    )),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+}
